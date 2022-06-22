9683cc21eb0df46e9f3dc57de1829bde586dc30e
yornvickie <yornvickie@gmail.com>
2022-06-22 12:18:42 -0500

    added Rock Paper Scissor Folder/game

---
+++ script.js
@@ -0,0 +1,56 @@
+let [computer_score,user_score] = [0,0];
+
+let result_ref = document.getElementById("result")
+let choices_object = {
+    'rock' : {
+        'rock' : 'draw',
+        'scissor' : 'win',
+        'paper' : 'lose'
+    },
+    'paper' : {
+        'rock' : 'win',
+        'scissor' : 'lose',
+        'paper' : 'draw'
+    },
+    'scissor' : {
+        'rock' : 'lose',
+        'scissor' : 'draw',
+        'paper' : 'win'
+    },
+
+
+}
+
+function checker(input){
+    let choices = ["rock", "paper", "scissor"];
+    let num = Math.floor(Math.random()*3);
+
+    document.getElementById("computer_choice").innerHTML = ` Computer chose <span>${choices[num].toUpperCase()} </span>`;
+
+    document.getElementById("user_choice").innerHTML = `You chose <span>${input.toUpperCase()} </span>`
+
+    let computer_choice = choices[num];
+
+    switch(choices_object[input][computer_choice]){
+        case 'win':
+            result_ref.style.cssText =
+            "background-color: #cefdce; color: #689f38";
+            result_ref.innerHTML = "YOU WIN";
+            user_score++;
+            break;
+        case 'lose':
+            result_ref.style.cssText =
+            "background-color: #ffdde0; color: #d32f2f";
+            result_ref.innerHTML = " YOU LOSE";
+            computer_score++;
+            break;
+        default:
+            result_ref.style.cssText =
+            "background-color: #e5e5e5; color #808080";
+            result_ref.innerHTML = "DRAW";
+            break;
+    }
+
+    document.getElementById("computer_score").innerHTML = computer_score;
+    document.getElementById("user_score").innerHTML = user_score;
+}
